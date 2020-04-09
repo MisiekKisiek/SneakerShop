93df13e5123c9cec3c44d2a63c324e1d76c24650
MisiekKisiek <skrzypkka@gmail.com>
2020-04-09 22:28:43 +0200

    Updates

---
+++ precache-manifest.e013f00f5fa7ec6cc9f1db3d5a9a265e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99605937e2e353061fa17f13971df6a5",
+    "url": "/SneakerShop/index.html"
+  },
+  {
+    "revision": "643971b0057d96646d2f",
+    "url": "/SneakerShop/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "2e2ae4879a518ec70dfd",
+    "url": "/SneakerShop/static/css/main.dfd729c7.chunk.css"
+  },
+  {
+    "revision": "643971b0057d96646d2f",
+    "url": "/SneakerShop/static/js/2.bf0d4e0d.chunk.js"
+  },
+  {
+    "revision": "150e7ebf1b16402b62fb665d1a7869df",
+    "url": "/SneakerShop/static/js/2.bf0d4e0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e2ae4879a518ec70dfd",
+    "url": "/SneakerShop/static/js/main.ab2de0d3.chunk.js"
+  },
+  {
+    "revision": "b54a8073df8438311ab8",
+    "url": "/SneakerShop/static/js/runtime-main.d582ed84.js"
+  },
+  {
+    "revision": "cd50d3029f0cee114eea08194c0669df",
+    "url": "/SneakerShop/static/media/1black-white.cd50d302.jpg"
+  },
+  {
+    "revision": "4eb06d7a85c26e4347fa6edeba214b2c",
+    "url": "/SneakerShop/static/media/logo100rose.4eb06d7a.png"
+  },
+  {
+    "revision": "e19f36e84c69cc6e12f59de792ee0808",
+    "url": "/SneakerShop/static/media/mainPageImg1.e19f36e8.jpg"
+  },
+  {
+    "revision": "1ad9d2997cba546717c07d9cda673d6a",
+    "url": "/SneakerShop/static/media/mainPageImg2.1ad9d299.jpg"
+  },
+  {
+    "revision": "ae61d9b324e774f0a650b23bf79ec49b",
+    "url": "/SneakerShop/static/media/mainPageImg3.ae61d9b3.jpg"
+  },
+  {
+    "revision": "b3ada815f7d669384db7c0c9b784f43e",
+    "url": "/SneakerShop/static/media/nike97black-white.b3ada815.jpg"
+  },
+  {
+    "revision": "117c7362d8a077994c62e7bd87cacd4a",
+    "url": "/SneakerShop/static/media/nike97white.117c7362.jpg"
+  },
+  {
+    "revision": "3f95b0b9a75e81cd8dc531dfe2924a1c",
+    "url": "/SneakerShop/static/media/nikenmdr1black-white.3f95b0b9.jpg"
+  },
+  {
+    "revision": "4f2cb9fe9d106216afd73dd4c7ddb2fd",
+    "url": "/SneakerShop/static/media/nikenmdr1purple-white.4f2cb9fe.jpg"
+  }
+]);
